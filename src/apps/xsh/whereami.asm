08450b5bc278cb031eaba86779e1e53b73c07526
autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-09-06 06:54:14 -0400

    Update whereami.asm

--- src/apps/xsh/whereami.asm
+++ src/apps/xsh/whereami.asm
@@ -1,1 +1,14 @@
+; =============================================================================
+; pwd — displays current directory [modular XSH command]
+; =============================================================================
+[BITS 64]
 
+xsh_cmd_whereami:
+    mov  bl, 0x0B
+    mov  rsi, msg_pipe
+    call xk_print
+    lea  rsi, [rel xsh_cwd_name]
+    call xk_print
+    mov  rsi, msg_pipe
+    call xk_println
+    ret
